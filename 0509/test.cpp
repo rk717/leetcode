4ad593d4640cf88748fb7d09e284eadfc1c81aa3
yilin <lianyilin98@gmail.com>
2021-04-15 09:28:03 +0200

    15

---
+++ 0509/test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std; 
+
+class Solution {
+public: 
+    int fib (int n) {
+        if (n == 0) {
+            return 0; 
+        }
+        if (n==1) {
+            return 1; 
+        }
+        else {
+            return fib(n-1) + fib(n-2);
+        }
+    }
+};
